545800e4120f0f85720db120e1f88483b4dc82a6
Nikhil Razz <nikhilhash03@gmail.com>
2026-05-27 10:56:06 +0530

    ex2

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Encapsulation
-{
-    class Account
-    {
-        private double balance;
-        public void deposit(double amount)
-        {
-            if (balance > 0)
-            {
-                balance += amount;
-            }
-        }
-        public void withdraw(double amount)
-        {
-            if (balance <= 0)
-            {
-                balance -= amount;
-            }
-        }
-        public double getbalance()
-        {
-            return balance;
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-                Account acc = new Account();
-                acc.deposit(5000);
-                acc.withdraw(2000);
+//namespace Encapsulation
+//{
+//    class Account
+//    {
+//        private double balance;
+//        public void deposit(double amount)
+//        {
+//            if (balance > 0)
+//            {
+//                balance += amount;
+//            }
+//        }
+//        public void withdraw(double amount)
+//        {
+//            if (balance <= 0)
+//            {
+//                balance -= amount;
+//            }
+//        }
+//        public double getbalance()
+//        {
+//            return balance;
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//                Account acc = new Account();
+//                acc.deposit(5000);
+//                acc.withdraw(2000);
 
-                Console.WriteLine("Balance "+ acc.getbalance());
-        }
-    }
-}
+//                Console.WriteLine("Balance "+ acc.getbalance());
+//        }
+//    }
+//}
---
+++ Encapsulation/Properties/ex2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Properties
+{
+    class Account
+    {
+        private double balance;
+        private int pin = 2001;
+        public void deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+        }
+        public void withdraw(double amount,int enteredpin)
+        {
+            if (amount <= 0 && pin == enteredpin)
+            {
+                balance -= amount;
+            }
+        }
+        public double getbalance()
+        {
+            return balance;
+        }
+    }
+    internal class ex2
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            acc.deposit(5000);
+            acc.withdraw(2000,2001);
+
+            Console.WriteLine("Balance " + acc.getbalance());
+        }
+    }
+}
+
+
